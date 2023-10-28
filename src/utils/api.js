e8ec829354a046aa2741490fe421ee84e0e3a519
Dhiraj Kaushik <dhiraj30498@gmail.com>
2023-10-29 04:27:44 +0530

    new chagesd

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from '../actions/types';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://devcommunity-7tqnuh759-dhiraj-kaushiks-projects.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
